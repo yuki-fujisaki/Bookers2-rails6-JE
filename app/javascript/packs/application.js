add51a601860d4493fc3898d4624ba01107b7f90
yuki-fujisaki <yuki.f.bizz@gmail.com>
2022-09-07 16:51:40 +0900

    [WIP]feature/google_maps_platform add js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,9 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import jQuery from "jquery"
+
+import "packs/jquery.jpostal.js"
+
 import "popper.js"
 import "bootstrap"
 
@@ -20,3 +23,18 @@ ActiveStorage.start()
 
 global.$ = jQuery;
 window.$ = jQuery;
+
+$(function() {
+    $(document).on('turbolinks:load', () => {
+      $('#user_postal_code').jpostal({
+        postcode : [
+          '#user_postal_code'
+        ],
+        address: {
+          "#user_prefecture_code": "%3", // # 都道府県が入力される
+          "#user_address"           : "%4%5", // # 市区町村と町域が入力される
+          "#user_street"         : "%6%7" // # 大口事務所の番地と名称が入力される
+        }
+      });
+    });
+  });
