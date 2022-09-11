3601b18dd07e695239588e1f111a9aba388b2ce7
yuki-fujisaki <yuki.f.bizz@gmail.com>
2022-09-11 12:01:47 +0900

    [WIP]feature/slick add slick

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,6 +14,8 @@ import "bootstrap"
 import '@fortawesome/fontawesome-free/js/all';
 import "../stylesheets/application" 
 
+import "./slick.js"
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
---
+++ app/javascript/packs/slick.js
@@ -0,0 +1,8 @@
+$(function() {
+    $('.slider').slick({
+        arrows: false,  //ボタン非表示
+        autoplay: true, //自動再生
+        autoplaySpeed: 4000, //再生スピード
+        dots: true, //ドット表示
+    });
+  });
